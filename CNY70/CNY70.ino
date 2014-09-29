58a09471a74665ed156e47b1427ba3c175c89db3
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2014-09-29 10:58:42 +0200

    S4A added

---
+++ CNY70/CNY70.ino
@@ -0,0 +1,16 @@
+// http://forum.arduino.cc/index.php?PHPSESSID=363paoco6fe4e6rf6vnq7uo580&topic=101156.0
+
+int input = A0;
+int val = 0;
+  
+void setup() {
+  Serial.begin(9600);
+  pinMode(input, INPUT);
+}
+
+void loop() {
+  val = analogRead(input);
+  Serial.println(val);
+  delay(500);
+}
+
